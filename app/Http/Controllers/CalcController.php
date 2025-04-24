59711c6678fb04d98563edc29cdc3c31b4eb3f20
Estevanzc <estevan.zimermann@gmail.com>
2025-04-24 11:42:26 -0300

    route and migration creation

--- app/Http/Controllers/CalcController.php
+++ app/Http/Controllers/CalcController.php
@@ -12,4 +12,17 @@ class CalcController extends Controller {
             "equals" => $n1 + $n2,
         ]);
     }
+    public function minus($n1, $n2) {
+        return view("minus", [
+            "n1" => $n1,
+            "n2" => $n2,
+            "equals" => $n1 - $n2,
+        ]);
+    }
+    public function power($n1) {
+        return view("power", [
+            "n1" => $n1,
+            "equals" => $n1**2,
+        ]);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,18 @@
 <?php
 
 use App\Http\Controllers\CalcController;
+use App\Http\Controllers\KeepinhoController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('index', ["name" => "estevan"]);
+Route::get('/', function() {
+    return view('index');
 });
-Route::get("/test", function() {
-    return view("index", [
-        "name" => "estevan",
-    ]);
-});
-Route::get("/test/{valor}", function($valor) {
-    return view("index", [
-        "name" => $valor,
-    ]);
-});
+
 Route::get("/soma/{n1}/{n2}", [CalcController::class, "plus"]);
+Route::get("/sub/{n1}/{n2}", [CalcController::class, "minus"]);
+Route::get("/power/{n1}", [CalcController::class, "power"]);
+
+Route::prefix("/keep")->group(function () {
+    Route::get('/', [KeepinhoController::class, "index"]);
+});
