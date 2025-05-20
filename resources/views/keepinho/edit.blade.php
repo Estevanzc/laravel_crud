e4ca4b48438303d89c06859a50cd5b37d8a089e2
Estevanzc <estevan.zimermann@gmail.com>
2025-05-20 09:03:59 -0300

    old value in edit

--- resources/views/keepinho/edit.blade.php
+++ resources/views/keepinho/edit.blade.php
@@ -14,15 +14,15 @@
         <input type="hidden" name="id" value="{{ $note->id }}">
         <div>
             <label for="title">Title</label>
-            <input type="text" id="title" name="title" value="{{ old("title", $note->title) }}" class="border-2 border-solid border-black">
+            <input type="text" id="title" name="title" value="{{ $note->title }}" class="border-2 border-solid border-black">
         </div>
         <div>
             <label for="texto">Text</label>
-            <textarea name="texto" id="texto" cols="30" rows="10" class="border-2 border-solid border-black">{{ old("texto", $note->texto) }}</textarea>
+            <textarea name="texto" id="texto" cols="30" rows="10" class="border-2 border-solid border-black">{{ $note->texto }}</textarea>
         </div>
         <div>
             <label for="altert">Alert date</label>
-            <input type="date" id="alert" name="alert" value="{{ old("alert", $note->alert) }}" class="border-2 border-solid border-black">
+            <input type="date" id="alert" name="alert" value="{{ $note->alert }}" class="border-2 border-solid border-black">
         </div>
         <button type="submit" class="border-2 border-solid border-black">Submit</button>
     </form>
