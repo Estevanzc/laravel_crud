f3df26d1210bca6cb6df68d8ea1a2b37a5b605ab
Estevanzc <estevan.zimermann@gmail.com>
2025-05-29 11:39:55 -0300

    email verification

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,11 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CalcController;
+use App\Http\Controllers\KeepinhoController;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Auth;
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -16,5 +21,20 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+Route::middleware(["auth"])->group(function () {
+    Route::get('/', [KeepinhoController::class, "index"])->name("keep.index");
+    Route::prefix("/keep")->group(function () {
+        Route::post('/insert', [KeepinhoController::class, "insert_data"])->name("keep.insert");
+        Route::get('/edit/{note}', [KeepinhoController::class, "edit_data"])->name("keep.edit");
+        Route::put('/update', [KeepinhoController::class, "update_data"])->name("keep.update");
+        Route::get('/delete/{note}', [KeepinhoController::class, "delete_data"])->name("keep.delete");
+        Route::get('/lixeira', [KeepinhoController::class, "trash"])->name("keep.trash");
+        Route::get('/restore/{note}', [KeepinhoController::class, "trash_restore"])->name("keep.restore");
+    });
+});
+
+Route::get("/soma/{n1}/{n2}", [CalcController::class, "plus"]);
+Route::get("/sub/{n1}/{n2}", [CalcController::class, "minus"]);
+Route::get("/power/{n1}", [CalcController::class, "power"]);
 
 require __DIR__.'/auth.php';
